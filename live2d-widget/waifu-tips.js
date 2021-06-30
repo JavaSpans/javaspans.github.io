4b5e9ebea83e782283ebd73df7ea7b905a999bb0
JavaSpans <21953238@qq.com>
2021-06-30 19:02:37 +0800

    Site updated: 2021-06-30 19:02:36

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -77,7 +77,7 @@ function loadWidget(config) {
 			Live2D.captureFrame = true;
 		});
 		document.querySelector("#waifu-tool .fa-info-circle").addEventListener("click", () => {
-			open("https://javaspans.github.io/blog/");
+			open("https://javaspans.github.io/");
 		});
 		document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
 			localStorage.setItem("waifu-display", Date.now());
